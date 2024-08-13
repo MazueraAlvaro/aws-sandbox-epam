ffacd1610085dec4cf8f35099a811bfffa75e242
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-13 01:05:58 -0500

    Add task02

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) => {
+  // TODO implement
+  if (event.requestContext.http.path == "/hello") {
+    return {
+      statusCode: 200,
+      message: "Hello from Lambda",
+    };
+  }
+  return {
+    statusCode: 400,
+    message: `Bad request syntax or unsupported method. Request path: ${event.requestContext.http.path}. HTTP method: ${event.requestContext.http.method}`,
+  };
+};
