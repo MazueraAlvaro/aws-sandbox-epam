ffa8a7cad531292da8771868c58c6a06d92f378e
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-13 01:26:51 -0500

    Fix task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
   // TODO implement
+  console.log(JSON.stringify(event))
   if (event.requestContext.http.path == "/hello") {
     return {
       statusCode: 200,
