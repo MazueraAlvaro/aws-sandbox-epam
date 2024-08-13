f79a8e838536d9fa35144bfed12a47cdaa553e6b
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-13 01:49:05 -0500

    Fix task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,14 +1,20 @@
 exports.handler = async (event) => {
   // TODO implement
-  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(event));
   if (event.requestContext.http.path == "/hello") {
     return {
       statusCode: 200,
-      message: "Hello from Lambda",
+      body: {
+        statusCode: 200,
+        message: "Hello from Lambda",
+      },
     };
   }
   return {
-    statusCode: 400,
-    message: `Bad request syntax or unsupported method. Request path: ${event.requestContext.http.path}. HTTP method: ${event.requestContext.http.method}`,
+    statusCode: 200,
+    body: {
+      statusCode: 400,
+      message: `Bad request syntax or unsupported method. Request path: ${event.requestContext.http.path}. HTTP method: ${event.requestContext.http.method}`,
+    },
   };
 };
