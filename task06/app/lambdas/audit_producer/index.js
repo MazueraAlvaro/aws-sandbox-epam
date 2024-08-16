ff0c687e5b83ae3abe6ab686c68c177f4d602065
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-15 23:41:05 -0500

    Add task06

---
+++ task06/app/lambdas/audit_producer/index.js
@@ -0,0 +1,46 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.table_name;
+
+
+const getParams = (itemKey, oldValue, newValue) => {
+    const createdAt = new Date();
+    return {
+		TableName: tableName,
+		Item: {
+			id: uuidv4(),
+            modificationTime: createdAt.toISOString(),
+            itemKey,
+            updatedAttribute: "value",
+            oldValue,
+            newValue
+		}
+	};
+	
+}
+
+exports.handler = async (event) => {
+    // TODO implement
+    console.log(JSON.stringify(event))
+    const record = event.Records[0];
+    if (record.eventName !== "MODIFY") return;
+    
+    const oldItem = event.dynamoDB.OldImage;
+    const newItem = event.dynamoDB.NewImage;
+    const oldValue = parseInt(oldItem.value.N);
+    const newValue = parseInt(newItem.value.N);
+    const itemKey = oldItem.key.S;
+    
+    
+    const params = getParams(itemKey, oldValue, newValue);
+    try {
+		await docClient.put(params).promise();
+		return true;
+	} catch (err) {
+        const parsedError = JSON.stringify(err, null, 2);
+        console.error(parsedError)
+		return parsedError;
+	}
+};
