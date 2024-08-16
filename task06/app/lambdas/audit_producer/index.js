dc9926eae8fbadd9719a99fe2d72605df84507ae
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-16 00:26:57 -0500

    Fix task06

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -4,43 +4,49 @@ const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.table_name;
 
-
-const getParams = (itemKey, oldValue, newValue) => {
-    const createdAt = new Date();
-    return {
-		TableName: tableName,
-		Item: {
-			id: uuidv4(),
-            modificationTime: createdAt.toISOString(),
-            itemKey,
-            updatedAttribute: "value",
-            oldValue,
-            newValue
-		}
-	};
-	
-}
+const getParams = (itemKey, newValue, oldValue = undefined) => {
+  const createdAt = new Date();
+  return {
+    TableName: tableName,
+    Item: {
+      id: uuidv4(),
+      modificationTime: createdAt.toISOString(),
+      itemKey,
+      updatedAttribute: "value",
+      ...(oldValue && { oldValue }),
+      newValue,
+    },
+  };
+};
 
 exports.handler = async (event) => {
-    // TODO implement
-    console.log(JSON.stringify(event))
-    const record = event.Records[0];
-    if (record.eventName !== "MODIFY") return;
-    
-    const oldItem = record.dynamodb.OldImage;
-    const newItem = record.dynamodb.NewImage;
-    const oldValue = parseInt(oldItem.value.N);
-    const newValue = parseInt(newItem.value.N);
-    const itemKey = oldItem.key.S;
-    
-    
-    const params = getParams(itemKey, oldValue, newValue);
-    try {
-		await docClient.put(params).promise();
-		return true;
-	} catch (err) {
-        const parsedError = JSON.stringify(err, null, 2);
-        console.error(parsedError)
-		return parsedError;
-	}
+  // TODO implement
+  console.log(JSON.stringify(event));
+  const record = event.Records[0];
+  if (!["MODIFY", "INSERT"].includes(record.eventName)) return;
+
+  const oldItem = record.dynamodb.OldImage;
+  const newItem = record.dynamodb.NewImage;
+  const oldValue = parseInt(oldItem.value.N);
+  const newValue = parseInt(newItem.value.N);
+  const itemKey = newItem.key.S;
+
+  let params = {};
+  if (record.eventName === "MODIFY") {
+    params = getParams(itemKey, newValue, oldValue);
+  } else {
+    params = getParams(itemKey, {
+      key: itemKey,
+      value: newValue,
+    });
+  }
+
+  try {
+    await docClient.put(params).promise();
+    return true;
+  } catch (err) {
+    const parsedError = JSON.stringify(err, null, 2);
+    console.error(parsedError);
+    return parsedError;
+  }
 };
