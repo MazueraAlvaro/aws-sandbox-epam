4a16808dcb62b34c7361998cb6e3f8feec69c6cb
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-16 21:42:41 -0500

    Add task07

---
+++ task07/app/lambdas/uuid_generator/index.js
@@ -0,0 +1,39 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const Readable = require('stream').Readable
+
+const s3 = new AWS.S3();
+
+const getFileContent = () => {
+    return {
+        ids: [...Array(10)].map(() => uuidv4())
+    }
+}
+
+const getRedableStream = () => {
+    const data = getFileContent();
+    return Readable.from([JSON.stringify(data)]);
+}
+
+exports.handler = async (event) => {
+    // TODO implement
+    console.log(JSON.stringify(event))
+
+    const params = {
+        Bucket: process.env.target_bucket,
+        Key: event.time,
+        Body: getRedableStream()
+    };
+
+    try {
+        await s3.upload(params).promise();
+    }
+    catch(e){
+        console.log(e);
+    }
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
