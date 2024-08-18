857e9c4aabc29a825aa9dd2815053232b5b1e4f9
Alvaro Mazuera U <am@alvaromazuera.com>
2024-08-18 14:41:34 -0500

    Add task09

---
+++ task09/app/lambdas/processor/index.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const axios = require("axios");
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.table_name;
+
+
+const getMeteo = () => {
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+    )
+    .then((data) => data.data);
+};
+
+const getParams = (forecast) => {
+    return {
+		TableName: tableName,
+		Item: {
+			"id": uuidv4(),
+			forecast
+		}
+	};
+	
+}
+
+exports.handler = async (event) => {
+  // TODO implement
+  const meteo = await getMeteo();
+  const params = getParams(meteo);
+
+  try {
+    await docClient.put(params).promise();
+    return {
+        statusCode: 201,
+        event: params.Item
+    };
+    } catch (err) {
+        return JSON.stringify(err, null, 2);
+    }
+};
